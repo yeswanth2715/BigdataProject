44fc0c6ba11684b2b70924118091d603e4974420
Yeswanth Reddy Jampala <jyeswanthreddy@gmail.com>
2025-01-27 23:19:19 +0530

    reducer.py

--- reducer.py
+++ reducer.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python
-
 import sys
 
-highest_score = float('-inf')
-lowest_score = float('inf')
-
+highest_salary = float('-inf')
+lowest_salary = float('inf')
 
 for line in sys.stdin:
-    line = line.strip()
-
     try:
-        score = float(line)
-        
-        
-        if score > highest_score:
-            highest_score = score
-        if score < lowest_score:
-            lowest_score = score
+        salary = float(line.strip())
+        highest_salary = max(highest_salary, salary)
+        lowest_salary = min(lowest_salary, salary)
     except ValueError:
-
-        sys.stderr.write("Skipping invalid score: {0}\n".format(line))
+        sys.stderr.write(f"Skipping invalid salary: {line}\n")
         continue
 
-print('Highest Score: {0}'.format(highest_score))
-print('Lowest Score: {0}'.format(lowest_score))
+print(f"Highest Salary: {highest_salary}")
+print(f"Lowest Salary: {lowest_salary}")
